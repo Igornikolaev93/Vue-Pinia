61b2b2b5f4dc8fa088d6fe07b22953f211621dd4
Igornikolaev93 <goscha.nikolae2015@yandex.ru>
2025-08-06 20:10:24 +0300

    Add files via upload
    type

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  
+      id: string,
+      title: string,
+      subtitle: string,
+      authors: string,
+      image: string,
+      url: string,
+      price:number,
+      
+         
+}
